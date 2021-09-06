5db16fdd1d74f1106b8258f0cdfd4f29a2191a2b
Abdiaziz Mohamed <faizuulbinmohammed6858@gmail.com>
2021-09-06 19:07:01 +0300

    eighth commit

--- app/src/main/java/com/example/tendoapp/data/model/ParentModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tendoapp.data.model
-
-data class ParentModel (
-    val title : String = "",
-    val children : List<ChildModel>
-)
--- app/src/main/java/com/example/tendoapp/ui/main/view/fragments/CategoriesFragment.kt
+++ app/src/main/java/com/example/tendoapp/ui/main/view/fragments/CategoriesFragment.kt
@@ -1,56 +1,53 @@
 package com.example.tendoapp.ui.main.view.fragments
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tendoapp.R
-import com.example.tendoapp.data.model.Category
+import com.example.tendoapp.data.model.Data
 import com.example.tendoapp.databinding.FragmentCategoriesBinding
-import com.example.tendoapp.databinding.FragmentInfoTabBinding
-import com.example.tendoapp.ui.main.adapter.CategoriesAdapter
-import com.example.tendoapp.ui.main.adapter.CategoriesAdapterHome
-import com.example.tendoapp.ui.main.adapter.CategoriesAdapterMain
-import com.example.tendoapp.ui.main.adapter.ParentAdapter
-import com.example.tendoapp.utils.objects.ParentDataFactory
+import com.example.tendoapp.ui.main.adapter.RecyclerViewAdapter
 import kotlinx.android.synthetic.main.fragment_categories.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class  CategoriesFragment : Fragment() {
 
-    lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerView: RecyclerView
     lateinit var binding: FragmentCategoriesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding= DataBindingUtil.inflate(inflater, R.layout.fragment_categories, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_categories, container, false)
 
-        initRecycler()
+
+        val dataList = ArrayList<Data>()
+        dataList.add(Data(RecyclerViewAdapter.VIEW_TYPE_ONE, "1. Hi! I am in View 1"))
+        dataList.add(Data(RecyclerViewAdapter.VIEW_TYPE_TWO, "2. Hi! I am in View 2"))
+        dataList.add(Data(RecyclerViewAdapter.VIEW_TYPE_ONE, "3. Hi! I am in View 3"))
+        dataList.add(Data(RecyclerViewAdapter.VIEW_TYPE_TWO, "4. Hi! I am in View 4"))
+        dataList.add(Data(RecyclerViewAdapter.VIEW_TYPE_ONE, "5. Hi! I am in View 5"))
+        dataList.add(Data(RecyclerViewAdapter.VIEW_TYPE_TWO, "6. Hi! I am in View 6"))
+        dataList.add(Data(RecyclerViewAdapter.VIEW_TYPE_ONE, "7. Hi! I am in View 7"))
+        dataList.add(Data(RecyclerViewAdapter.VIEW_TYPE_TWO, "8. Hi! I am in View 8"))
+        dataList.add(Data(RecyclerViewAdapter.VIEW_TYPE_ONE, "9. Hi! I am in View 9"))
+        dataList.add(Data(RecyclerViewAdapter.VIEW_TYPE_TWO, "10. Hi! I am in View 10"))
+        dataList.add(Data(RecyclerViewAdapter.VIEW_TYPE_ONE, "11. Hi! I am in View 11"))
+        dataList.add(Data(RecyclerViewAdapter.VIEW_TYPE_TWO, "12. Hi! I am in View 12"))
+
+        recyclerView = binding.recyclerViewCategory
+
+        val adapter = RecyclerViewAdapter(requireContext(), dataList)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = adapter
 
         return binding.root
-    }
-    @SuppressLint("WrongConstant")
-
-    private fun initRecycler(){
-        recyclerView = rv_parent
-
-        recyclerView.apply {
-            layoutManager = LinearLayoutManager(requireContext(),
-                LinearLayout.VERTICAL, false)
-            adapter = ParentAdapter(
-                ParentDataFactory
-                .getParents(5))
-        }
 
     }
-
-
-    }
+}
